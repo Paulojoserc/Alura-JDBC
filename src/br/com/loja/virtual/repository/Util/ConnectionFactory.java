e5f03543a45c568910243bf0cd0fbfb89af9681c
Paulojoserc <pjrc83@gmail.com>
2023-04-18 23:46:09 -0300

    Inserindo com Statement

--- src/br/com/loja/virtual/repository/Util/ConnectionFactory.java
+++ src/br/com/loja/virtual/repository/Util/ConnectionFactory.java
@@ -9,5 +9,7 @@ public class ConnectionFactory {
 	public Connection recuperarConexao() throws SQLException {
 		return DriverManager.getConnection(
 				"jdbc:mysql://localhost:3307/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "Alura123");
+		
+		
 }
 }
