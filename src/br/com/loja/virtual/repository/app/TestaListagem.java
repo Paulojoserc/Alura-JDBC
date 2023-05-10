d0a037c6cdf72e948414b6da1fa83e643e13ed15
Paulojoserc <pjrc83@gmail.com>
2023-05-10 00:12:56 -0300

    Listagem e remoção
    Ao executar SQL como Statement, temos um risco de segurança, chamado de SQL Injection
    SQL Injection nada mais é do que passar um novo comando SQL como parâmetro
    Para evitar SQL Injection, devemos usar a interface PreparedStatement
    Diferentemente do Statement, o PreparedStatement trata (sanitiza) cada parâmetro do comando SQL

--- src/br/com/loja/virtual/repository/app/TestaListagem.java
+++ src/br/com/loja/virtual/repository/app/TestaListagem.java
@@ -1,9 +1,9 @@
 package br.com.loja.virtual.repository.app;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import br.com.loja.virtual.repository.Util.ConnectionFactory;
 
@@ -14,8 +14,8 @@ public class TestaListagem {
 		Connection connection = connectionFactory.recuperarConexao();
 		System.out.println("Fechando conexão!");
 
-		Statement stm = connection.createStatement();
-		stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+		PreparedStatement stm = connection.prepareStatement("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+		
 
 		ResultSet rst = stm.getResultSet();
 
