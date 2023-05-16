4958fda6534fd826701ae30aedcdff58ad3e26c3
Paulojoserc <pjrc83@gmail.com>
2023-05-15 22:23:07 -0300

    Usando o try-with-resources
    O banco de dados oferece um recurso chamado de transação, para juntar várias alterações como unidade de trabalho
    Se uma alteração falha, nenhuma alteração é aplicada (é feito um rollback da transação)
    Todas as alterações precisam funcionar para serem aceitas (é feito um commit)
    commit e rollback são operações clássicas de transações
    Para garantir o fechamento dos recursos, existe no Java uma cláusula try-with-resources
    O recurso em questão deve usar a interface Autoclosable

--- src/br/com/loja/virtual/repository/app/TestaInsercaoComParametro.java
+++ src/br/com/loja/virtual/repository/app/TestaInsercaoComParametro.java
@@ -10,46 +10,45 @@ import br.com.loja.virtual.repository.Util.ConnectionFactory;
 
 public class TestaInsercaoComParametro {
 	public static void main(String[] args) throws SQLException {
-		String nome ="Mouse'";
-		String descricao="Mouse sem fio); delete from Produto;" ;
+		String nome = "Mouse'";
+		String descricao = "Mouse sem fio); delete from Produto;";
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		Connection connection = connectionFactory.recuperarConexao();
-		connection.setAutoCommit(false);
-		
-		try {
-			PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO ( nome, descricao) VALUES (? , ?)",Statement.RETURN_GENERATED_KEYS);
-			
-			adicionarVariavel("SmartTv", "45 polegadas", stm);
-			adicionarVariavel("Radio", "Radio de bateria", stm);
-			connection.commit();
-			
-			stm.close();
-			connection.close();
-		}catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("ROLLBACK EXECUTADO");
-			connection.rollback();
+		try (Connection connection = connectionFactory.recuperarConexao()) {
+			connection.setAutoCommit(false);
+
+			try (PreparedStatement stm = connection.prepareStatement(
+					"INSERT INTO PRODUTO ( nome, descricao) VALUES (? , ?)", Statement.RETURN_GENERATED_KEYS)) {
+				adicionarVariavel("SmartTv", "45 polegadas", stm);
+				adicionarVariavel("Radio", "Radio de bateria", stm);
+				connection.commit();
+
+				connection.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				connection.rollback();
+			}
 		}
-		
+
 	}
-	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException{
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
-		if (nome.equalsIgnoreCase("Radio")){
-			
+		if (nome.equalsIgnoreCase("Radio")) {
+
 		}
-		if(nome.equals("Radio")) {
+		if (nome.equals("Radio")) {
 			throw new RuntimeException("Não foi possivel adicionar o produto");
 		}
-		stm.execute( ); 
+		stm.execute();
 
-
-		ResultSet rst = stm.getGeneratedKeys();
-		
-		while(rst.next()) {
-			Integer id = rst.getInt(1);
-			System.out.println("O id criado foi: "+id);
+		try (ResultSet rst = stm.getGeneratedKeys()) {
+			while (rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("O id criado foi: " + id);
+			}
+			rst.close();
 		}
-		rst.close();
 	}
 }
