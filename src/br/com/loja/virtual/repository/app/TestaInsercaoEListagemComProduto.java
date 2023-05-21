d0f383dede761b68928e2ecf29892a80bc6fc55d
Paulojoserc <pjrc83@gmail.com>
2023-05-21 11:08:10 -0300

    correção de classe Produto Dao

--- src/br/com/loja/virtual/repository/app/TestaInsercaoEListagemComProduto.java
+++ src/br/com/loja/virtual/repository/app/TestaInsercaoEListagemComProduto.java
@@ -11,15 +11,14 @@ import br.com.loja.virtual.repository.modelo.Produto;
 public class TestaInsercaoEListagemComProduto {
 
 	public static void main(String[] args) throws SQLException {
+
 		Produto comoda = new Produto("Cômoda", "Cômoda Vertical");
 
-		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
-			ProdutoDAO produtoDAO = new ProdutoDAO(connection);
-			produtoDAO.salvar(comoda);
-			List<Produto> listaDeProdutos = produtoDAO.listar();
+		try(Connection connection = new ConnectionFactory().recuperarConexao()) {
+			ProdutoDAO produtoDao = new ProdutoDAO(connection);
+			produtoDao.salvar(comoda);
+			List<Produto> listaDeProdutos = produtoDao.listar();
 			listaDeProdutos.stream().forEach(lp -> System.out.println(lp));
-			
 		}
-		System.out.println(comoda);
 	}
 }
--- src/br/com/loja/virtual/repository/dao/ProdutoDAO.java
+++ src/br/com/loja/virtual/repository/dao/ProdutoDAO.java
@@ -11,6 +11,7 @@ import java.util.List;
 import br.com.loja.virtual.repository.modelo.Produto;
 
 public class ProdutoDAO {
+
 	private Connection connection;
 
 	public ProdutoDAO(Connection connection) {
@@ -20,29 +21,34 @@ public class ProdutoDAO {
 	public void salvar(Produto produto) throws SQLException {
 		String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)";
 
-		try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+		try(PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
 			pstm.setString(1, produto.getNome());
 			pstm.setString(2, produto.getDescricao());
 
 			pstm.execute();
 
-			try (ResultSet rst = pstm.getGeneratedKeys()) {
-				while (rst.next()) {
+			try(ResultSet rst = pstm.getGeneratedKeys()) {
+				while(rst.next()) {
 					produto.setId(rst.getInt(1));
 				}
 			}
 		}
 	}
-	public List<Produto> listar() throws SQLException{
-		List<Produto> produtos =  new ArrayList<Produto>();
-		
-		String sql = "SELECT ID, Nome, Descricao from Produto";
-		
-		try(PreparedStatement pstm = connection.prepareStatement(sql)){
-			try(ResultSet rst = pstm.getResultSet()){
+
+	public List<Produto> listar() throws SQLException {
+		List<Produto> produtos = new ArrayList<Produto>();
+
+		String sql = "SELECT ID, NOME, DESCRICAO FROM PRODUTO";
+
+		try(PreparedStatement pstm = connection.prepareStatement(sql)) {
+			pstm.execute();
+
+			try(ResultSet rst = pstm.getResultSet()) {
 				while(rst.next()) {
-					Produto produto = new Produto(rst.getInt(1), rst.getString(2), rst.getString(3));
-					
+					Produto produto = 
+							new Produto(rst.getInt(1), rst.getString(2), rst.getString(3));
+
 					produtos.add(produto);
 				}
 			}
