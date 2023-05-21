fbac6603dda96706afab1a050d4c67c7dafc39fa
Paulojoserc <pjrc83@gmail.com>
2023-05-21 10:14:28 -0300

    Listando no ProdutoDAO
    Para cada tabela de domínio, temos uma classe de domínio
    Por exemplo, a tabela produtos tem uma classe Produto associada
    Objetos dessa classe representa um registro na tabela
    Para acessar a tabela, usaremos um padrão chamado Data Access Object (DAO)
    Para cada classe de domínio, existe um DAO. Por exemplo, a classe Produto possui um ProdutoDao
    Todos os métodos JDBC relacionados com o produto devem estar encapsulados no ProdutoDao

--- src/br/com/loja/virtual/repository/app/TestaInsercaoEListagemComProduto.java
+++ src/br/com/loja/virtual/repository/app/TestaInsercaoEListagemComProduto.java
@@ -2,12 +2,13 @@ package br.com.loja.virtual.repository.app;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 import br.com.loja.virtual.repository.Util.ConnectionFactory;
 import br.com.loja.virtual.repository.dao.ProdutoDAO;
 import br.com.loja.virtual.repository.modelo.Produto;
 
-public class TestaInsercaoComProduto {
+public class TestaInsercaoEListagemComProduto {
 
 	public static void main(String[] args) throws SQLException {
 		Produto comoda = new Produto("Cômoda", "Cômoda Vertical");
@@ -15,6 +16,8 @@ public class TestaInsercaoComProduto {
 		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
 			ProdutoDAO produtoDAO = new ProdutoDAO(connection);
 			produtoDAO.salvar(comoda);
+			List<Produto> listaDeProdutos = produtoDAO.listar();
+			listaDeProdutos.stream().forEach(lp -> System.out.println(lp));
 			
 		}
 		System.out.println(comoda);
